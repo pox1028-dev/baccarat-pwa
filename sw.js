20b2ad9eff3aec8a7bdb833fef65965dbb3fe235
pox1028-dev <pox1028@gmail.com>
2025-08-31 12:36:59 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('baccarat-mc-v3').then(cache=>cache.addAll(['./','./index_v3.html','./manifest_v3.webmanifest'])));
-});
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(caches.match(e.request).then(resp=> resp || fetch(e.request)));
-});
